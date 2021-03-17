1db3aee0150e01366de9dbcfe6e81ab7eff32522
John Fereira <jaf30@cornell.edu>
2021-03-17 11:36:36 -0400

    mark as deprecated

--- src/main/java/org/olf/folio/order/OrderImportShortened.java
+++ src/main/java/org/olf/folio/order/OrderImportShortened.java
@@ -45,6 +45,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 
+@Deprecated
 public class OrderImportShortened {
 	
 	private static final Logger logger = Logger.getLogger(OrderImportShortened.class);
