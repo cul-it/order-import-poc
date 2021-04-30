377785851071da43009032165537a4595896f91f
John Fereira <jaf30@cornell.edu>
2021-04-30 13:48:29 -0400

    add init method to get buildDir

--- src/test/java/org/olf/folio/order/util/MarcUtilsBaseTest.java
+++ src/test/java/org/olf/folio/order/util/MarcUtilsBaseTest.java
@@ -5,30 +5,38 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Test;
+import org.apache.commons.configuration.CompositeConfiguration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.lang3.StringUtils;
 import org.marc4j.MarcReader;
-import org.marc4j.MarcStreamReader;
-import org.marc4j.marc.DataField;
+import org.marc4j.MarcStreamReader; 
 import org.marc4j.marc.Record;
-
-public class MarcUtilsBaseTest { 
-	protected String file1 = "/Volumes/LaCie/cul/src/order-import-poc-dev/marc-test-files/harrass.mrc"; 
-	protected String file2 = "/Volumes/LaCie/cul/src/order-import-poc-dev/marc-test-files/Casalini.1.mrc";
-	protected String file3 = "/Volumes/LaCie/cul/src/order-import-poc-dev/marc-test-files/physical.mrc";
-	protected String file4 = "/Volumes/LaCie/cul/src/order-import-poc-dev/test-mrc/AmazonFO.1.mrc";
-	protected String file5 = "/Volumes/LaCie/cul/src/order-import-poc-dev/test-mrc/Midwest.1.mrc";
-	protected String file6 = "/Volumes/LaCie/cul/src/order-import-poc-dev/test-mrc/CouttsUKFO.1.mrc";
-	protected String file7 = "/Volumes/LaCie/cul/src/order-import-poc-dev/test-mrc/requesters_5-records_2021-03-11.mrc";
+ 
+ 
+public class MarcUtilsBaseTest {
 	
+	private String buildDir;
 	
-	public String[] fileArray = {
-		"/Volumes/LaCie/cul/src/order-import-poc-dev/marc-test-files/harrass.mrc",
-	};  
-    
-	public List<String> files = Arrays.asList(fileArray);
-	
+	protected String file1; 
+	protected String file2;
+	protected String file3;
+	protected String file4;
+	protected String file5;
+	protected String file6;
+	protected String file7;
+	 
 	MarcUtils marcUtils = new MarcUtils();
-
+	public MarcUtilsBaseTest() {
+		init(); 
+		this.file1 = this.buildDir +"/marc-test-files/harrass.mrc"; 
+		this.file2 = this.buildDir +"/marc-test-files/Casalini.1.mrc";
+		this.file3 = this.buildDir +"/marc-test-files/physical.mrc";
+		this.file4 = this.buildDir +"/marc-test-files/AmazonFO.1.mrc";
+		this.file5 = this.buildDir +"/marc-test-files/MIDWEST.1.mrc";
+		this.file6 = this.buildDir +"/marc-test-files/CouttsUKFO.1.mrc";
+		this.file7 = this.buildDir +"/marc-test-files/requesters_5-records_2021-03-11.mrc";
+	}
 	 
 	public List<Record> getRecords(String fname) throws Exception{
 		List<Record> records = new ArrayList<Record>();
@@ -41,5 +49,23 @@ public class MarcUtilsBaseTest {
 		}
 		return records;
 	}
-
+	
+    public void init() {
+    	CompositeConfiguration config = new CompositeConfiguration();
+    	PropertiesConfiguration props = new PropertiesConfiguration();
+    	
+		String use_env = System.getenv("USE_SYSTEM_ENV");
+		if (StringUtils.isNotEmpty(use_env) && StringUtils.equals(use_env, "true")) {
+			config.setProperty("buildDir",  System.getenv("buildDir")); 
+		 	
+		} else { 
+		   try {
+			   props.load(ClassLoader.getSystemResourceAsStream("application.properties"));
+			   this.buildDir = (String) props.getProperty("buildDir"); 
+		   } catch (ConfigurationException e) { 
+		      throw new RuntimeException("Could not load application.properties file");
+		   }
+		   config.addConfiguration(props);
+		}
+    }
 }
