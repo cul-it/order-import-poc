47101fe83fb59fb2b2fd280cbf23edee58832057
John Fereira <jaf30@cornell.edu>
2021-06-09 10:16:18 -0400

    addded BuildIdentifier, BuildContributor tests and made sure debug is set to false for all tests

--- src/test/java/org/olf/folio/order/util/MarcUtilsBuildContributorsTest.java
+++ src/test/java/org/olf/folio/order/util/MarcUtilsBuildContributorsTest.java
@@ -5,7 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
@@ -15,15 +18,18 @@ import org.marc4j.marc.Record;
  * @author jaf30
  *
  */
-public class MarcUtilsIsbnTest extends MarcUtilsBaseTest { 
+public class MarcUtilsBuildContributorsTest extends MarcUtilsBaseTest { 
     
     boolean debug = false;
-
+    String personalNameTypeId = "2b94c631-fca9-4892-a730-03ee529ffe2a";
+    
     @Test
-    public void testGetIsbn() {
+    public void testBuildContributors() {
+        HashMap<String, String> lookupTable = new HashMap<String, String>();
+        //lookupTable.put("bkp", "8261054f-be78-422d-bd51-4ed9f33c3422");
          
         List<String> myFnames = new ArrayList<String>();
-        myFnames.add(this.harrassowitz);
+        myFnames.add(this.amazonFO);
         
         try {
             for (String fname : myFnames) {
@@ -31,13 +37,13 @@ public class MarcUtilsIsbnTest extends MarcUtilsBaseTest {
                 List<Record> records = getRecords(fname);
                 for (Record record : records) {
                     JSONObject jsonObj = new JSONObject();
-                    JSONArray productIds = marcUtils.getISBN(record, "8261054f-be78-422d-bd51-4ed9f33c3422");
-                    jsonObj.put("productIds", productIds);
+                    JSONArray contributors = marcUtils.buildContributors(record, lookupTable);
+                    jsonObj.put("contributors", contributors);
                     if (debug) {
                         System.out.println(jsonObj.toString(3));
                     } else {
-                        assertNotNull(productIds);
-                        assertTrue(productIds.length() > 0);
+                        assertNotNull(contributors);
+                        assertTrue(contributors.length() > 0);
                     }
                 } 
             }
